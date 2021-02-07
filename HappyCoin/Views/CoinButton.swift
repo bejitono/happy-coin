d496de50b68c1a053591110813d300a424ede7df
bejitono <stdemicheli@protonmail.com>
2021-02-07 21:33:19 +0700

    Implements button.

---
+++ HappyCoin/Views/CoinButton.swift
@@ -0,0 +1,38 @@
+//
+//  CoinButton.swift
+//  HappyCoin
+//
+//  Created by Stefano on 07.02.21.
+//
+
+import SwiftUI
+
+struct CoinButton: View {
+        
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action, label: {
+            Text(title)
+                .foregroundColor(.black)
+                .font(.title2)
+                .bold()
+                .minimumScaleFactor(.scaleFactor)
+                .lineLimit(1)
+                .padding(.padding)
+        })
+        .frame(maxWidth: .infinity, maxHeight: .height)
+        .background(Color.gray)
+        .cornerRadius(.greatestFiniteMagnitude)
+    }
+}
+
+// MARK: - Constants
+
+private extension CGFloat {
+    static let lineLimit: CGFloat = 1
+    static let padding: CGFloat = 10
+    static let height: CGFloat = 50
+    static let scaleFactor: CGFloat = 0.5
+}
